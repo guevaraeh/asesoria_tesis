39333fbf3b8b270ad02364b5bd58e7defc757a47
elard <guevara.eh@gmail.com>
2025-07-24 12:08:34 -0500

    miembro de equipo

--- resources/views/admin/about/team_member.blade.php
+++ resources/views/admin/about/team_member.blade.php
@@ -72,48 +72,58 @@
                     </div>
 
 
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label"><b>Correo Electrónico</b></label>
+                                <input type="email" name="email" class="form-control" placeholder="correo@ejemplo.com" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            @if(count($emails) > 0)
+                            <div class="mb-3">
+                                <label><b>Principal</b></label>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="main" id="inlineRadio1" value="0" checked>
+                                    <label class="form-check-label" for="inlineRadio1">No</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="main" id="inlineRadio2" value="1">
+                                    <label class="form-check-label" for="inlineRadio2">Si</label>
+                                </div>
+                            </div>
+                            @else
+                            <input type="hidden" name="main" value="1" required>
+                            @endif
+                        </div>
+                    </div>
 
-                    <div class="mb-3">
-                        <label class="form-label"><b>Correo Electrónico</b></label>
-                        <input type="email" name="email" class="form-control" placeholder="correo@ejemplo.com" required>
-                    </div>
-                    @if(count($emails) > 0)
-                    <div class="mb-3">
-                        <label><b>Principal</b></label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="main" id="inlineRadio1" value="0" checked>
-                            <label class="form-check-label" for="inlineRadio1">No</label>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label"><b>Número de Teléfono</b></label>
+                                <input type="tel" pattern="[0-9]{9}" name="number" class="form-control" required>
+                            </div>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="main" id="inlineRadio2" value="1">
-                            <label class="form-check-label" for="inlineRadio2">Si</label>
+                        <div class="col-lg-6">
+                            @if(count($phones) > 0)
+                            <div class="mb-3">
+                                <label><b>Principal</b></label>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="main" id="inlineRadio1" value="0" checked>
+                                    <label class="form-check-label" for="inlineRadio1">No</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="main" id="inlineRadio2" value="1">
+                                    <label class="form-check-label" for="inlineRadio2">Si</label>
+                                </div>
+                            </div>
+                            @else
+                            <input type="hidden" name="main" value="1" required>
+                            @endif
                         </div>
                     </div>
-                    @else
-                    <input type="hidden" name="main" value="1" required>
-                    @endif
 
-                    <div class="mb-3">
-                        <label class="form-label"><b>Número de Teléfono</b></label>
-                        <input type="tel" pattern="[0-9]{9}" name="number" class="form-control" required>
-                    </div>
-                    @if(count($phones) > 0)
-                    <div class="mb-3">
-                        <label><b>Principal</b></label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="main" id="inlineRadio1" value="0" checked>
-                            <label class="form-check-label" for="inlineRadio1">No</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="main" id="inlineRadio2" value="1">
-                            <label class="form-check-label" for="inlineRadio2">Si</label>
-                        </div>
-                    </div>
-                    @else
-                    <input type="hidden" name="main" value="1" required>
-                    @endif
-
-                    
 
                     <div class="mb-3">
                         <label class="form-label"><b>Foto</b></label>
